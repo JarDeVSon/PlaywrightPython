aaf58b0c3f520de1e47b6366254593a9b7f48678
JarDeVSon <jardounifanor@gmail.com>
2026-03-02 17:23:46 -0300

    go

--- tests/test_example.py
+++ tests/test_example.py
@@ -1,5 +1,5 @@
 import re
-from playwright.sync_api import *
+from playwright.sync_api import expect
 
 def test_has_title(page):
     page.goto("https://playwright.dev/")
@@ -14,4 +14,4 @@ def test_get_started_link(page):
     page.get_by_role("link", name="Get started").click()
 
     # Expects page to have a heading with the name of Installation.
-    expect(page.get_by_role("heading", name="Installation")).to_be_visible()
+    expect(page.get_by_role("heading", name="InstallationMMMM")).to_be_visible()
