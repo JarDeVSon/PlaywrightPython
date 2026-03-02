93d2d89bd471cfdcc9c47cbaa62d6fb17cd7251d
JarDeVSon <jardounifanor@gmail.com>
2026-03-02 17:34:47 -0300

    fix

--- tests/test_example.py
+++ tests/test_example.py
@@ -14,4 +14,4 @@ def test_get_started_link(page):
     page.get_by_role("link", name="Get started").click()
 
     # Expects page to have a heading with the name of Installation.
-    expect(page.get_by_role("heading", name="InstallationMMMM")).to_be_visible()
+    expect(page.get_by_role("heading", name="Installation")).to_be_visible()
